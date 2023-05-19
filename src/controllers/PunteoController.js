40823e9944da50038d93000b0c8275b83a20882e
Luis Monterroso <namenotfound4004@gmail.com>
2023-05-19 14:21:33 -0600

    Medallas he historial implementado

--- src/controllers/PunteoController.js
+++ src/controllers/PunteoController.js
@@ -13,7 +13,7 @@ const traerRankingDeUnJuego = async (req, res) => {
 
     const ranking = await Punteo.aggregate([
         { $match: { codigoDelJuego: idConvert } },//filtra por fechas de compra
-        { $sort: {  punteo: -1, tiempo: 1 } },
+        { $sort: { punteo: -1, tiempo: 1 } },
         { $limit: 10 }
     ])
 
@@ -21,6 +21,88 @@ const traerRankingDeUnJuego = async (req, res) => {
 
 }
 
+
+
+const traerHistorialDeJuego = async (req, res) => {
+    const nombreUsuario = req.query.usuario
+
+    if (!nombreUsuario || nombreUsuario == "") {
+        res.json({});
+    }
+
+    const historial = await Punteo.aggregate([
+        { $lookup: { from: 'juegos', localField: 'codigoDelJuego', foreignField: '_id', as: 'juego' } },
+        { $match: { usuarioJugador: nombreUsuario } },//filtra por fechas de compra
+        { $sort: { tiempo: -1 } },
+    ])
+
+    res.json({ historial });
+
+}
+
+const traerMedallasDeUsuario = async (req, res) => {
+    const nombreUsuario = req.query.usuario //traer el parametro del nombre de usuario dentro de url
+    const medallasUsuario = [];
+    if (!nombreUsuario || nombreUsuario == "") {//comparamos si es vacio o nulo
+        res.json({});
+    }
+
+
+    //Para juego ordena palabra
+    const totalPuntosOrdenaPalabra = await Punteo.aggregate([
+        { $lookup: { from: 'juegos', localField: 'codigoDelJuego', foreignField: '_id', as: 'juego' } },
+        { $unwind: "$juego" }, //desenvuelve todos los arrays de articulos de los pedidos que coincidan en fechas
+        { $match: { usuarioJugador: nombreUsuario, "juego.tipo": "Ordena palabra" } },//filtra por fechas de compra
+        { $group: { _id: "juego.tipo", punteo: { $sum: "$punteo" }/*devuelve la informacion del primer elemento de group*/ } },
+    ])
+
+    if (totalPuntosOrdenaPalabra[0].punteo >= 10) {
+        medallasUsuario.push({ nombreMedalla: "Medalla de juego Ordena Palabra", imgMedalla: "medalla3", punteoTotal: totalPuntosOrdenaPalabra[0].punteo });
+    }
+
+    //Para juego ahorcado
+    const totalPuntosAhorcado = await Punteo.aggregate([
+        { $lookup: { from: 'juegos', localField: 'codigoDelJuego', foreignField: '_id', as: 'juego' } },
+        { $unwind: "$juego" }, //desenvuelve todos los arrays de articulos de los pedidos que coincidan en fechas
+        { $match: { usuarioJugador: nombreUsuario, "juego.tipo": "Ahorcado" } },//filtra por fechas de compra
+        { $group: { _id: "juego.tipo", punteo: { $sum: "$punteo" }/*devuelve la informacion del primer elemento de group*/ } },
+    ])
+
+    if (totalPuntosAhorcado[0].punteo >= 10) {
+        medallasUsuario.push({ nombreMedalla: "Medalla de juego Ahorcado", imgMedalla: "medalla1", punteoTotal: totalPuntosAhorcado[0].punteo });
+    }
+
+
+    //Para juego ahorcado
+    const totalPuntosQuiz = await Punteo.aggregate([
+        { $lookup: { from: 'juegos', localField: 'codigoDelJuego', foreignField: '_id', as: 'juego' } },
+        { $unwind: "$juego" }, //desenvuelve todos los arrays de articulos de los pedidos que coincidan en fechas
+        { $match: { usuarioJugador: nombreUsuario, "juego.tipo": "Quiz" } },//filtra por fechas de compra
+        { $group: { _id: "juego.tipo", punteo: { $sum: "$punteo" }/*devuelve la informacion del primer elemento de group*/ } },
+    ])
+
+    if (totalPuntosQuiz[0].punteo >= 10) {
+        medallasUsuario.push({ nombreMedalla: "Medalla de juego Quiz", imgMedalla: "medalla4", punteoTotal: totalPuntosQuiz[0].punteo });
+    }
+
+
+    //Para juego ahorcado
+    const totalPuntosMemoria = await Punteo.aggregate([
+        { $lookup: { from: 'juegos', localField: 'codigoDelJuego', foreignField: '_id', as: 'juego' } },
+        { $unwind: "$juego" }, //desenvuelve todos los arrays de articulos de los pedidos que coincidan en fechas
+        { $match: { usuarioJugador: nombreUsuario, "juego.tipo": "Memoria" } },//filtra por fechas de compra
+        { $group: { _id: "juego.tipo", punteo: { $sum: "$punteo" }/*devuelve la informacion del primer elemento de group*/ } },
+    ])
+
+    if (totalPuntosMemoria[0].punteo >= 1000) {
+
+        medallasUsuario.push({ nombreMedalla: "Medalla de juego Memoria", imgMedalla: "medalla2", punteoTotal: totalPuntosMemoria[0].punteo });
+    }
+    res.json({ medallasUsuario });
+}
+
 module.exports = {
     traerRankingDeUnJuego: traerRankingDeUnJuego,
+    traerHistorialDeJuego: traerHistorialDeJuego,
+    traerMedallasDeUsuario: traerMedallasDeUsuario
 }
--- src/routes/Punteo.routes.js
+++ src/routes/Punteo.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 
 router.get('/traerRankingDeUnJuego', punteoController.traerRankingDeUnJuego);
-
-
+router.get('/traerHistorialDeJuego', punteoController.traerHistorialDeJuego);
+router.get('/traerMedallasDeUsuario', punteoController.traerMedallasDeUsuario);
 module.exports = router;//exporar el routers
